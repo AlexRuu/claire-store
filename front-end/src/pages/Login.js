646c5e0363d522283cb7dd8cdb1f6ad40019969c
AlexRuu <weiru@hotmail.ca>
2023-02-21 17:52:34 -0500

    Add login functionality

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,7 +1,57 @@
 import { useState } from "react";
+import axios from "axios";
+import FormRow from "../components/FormRow";
+import { useAuthContext } from "../context/auth-context";
+import { useNavigate, Link } from "react-router-dom";
 
 const Login = () => {
-    const []
+  const { saveUser } = useAuthContext();
+  const navigate = useNavigate();
+  const [values, setValues] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password } = values;
+    const loginUser = { email, password };
+    try {
+      const data = await axios.post(
+        "http://localhost:4000/api/auth/login",
+        loginUser
+      );
+      setValues({ email: "", password: "" });
+      saveUser(data.user);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <section>
+      <form className="login-form" onSubmit={onSubmit}>
+        <FormRow
+          type="email"
+          name="email"
+          values={values.email}
+          onChange={handleChange}
+        />
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+        <p>
+          <Link to="/register">Register</Link>
+        </p>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
