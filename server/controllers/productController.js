48a73973e1aff3fe500990488cc9587e4c05ecb1
AlexRuu <weiru@hotmail.ca>
2023-02-24 21:29:06 -0500

    Add product controllers

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,17 +5,55 @@ const path = require("path");
 
 const getAllProducts = async (req, res) => {
   const products = await Product.find({});
-
   res.status(StatusCodes.OK).json({ products, count: products.length });
 };
 
-const getSingleProduct = async (req, res) => {};
+const getSingleProduct = async (req, res) => {
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
 
-const createProduct = async (req, res) => {};
+  if (!product) {
+    throw new CustomError.NotFoundError(
+      "There doesn't seem to be a product here..."
+    );
+  }
+  res.status(StatusCodes.OK).json({ product });
+};
 
-const updateProduct = async (req, res) => {};
+const createProduct = async (req, res) => {
+  req.body.user = req.user.userId;
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({ product });
+};
 
-const deleteProduct = async (req, res) => {};
+const updateProduct = async (req, res) => {
+  const { id: productId } = req.params;
+
+  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!product) {
+    throw new CustomError.NotFoundError("This product is not availabl...");
+  }
+
+  res.status(StatusCodes.OK).json({ product });
+};
+
+const deleteProduct = async (req, res) => {
+  const { id: productId } = req.params;
+
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError("This product is not available...");
+  }
+
+  await product.remove();
+
+  res.status(StatusCodes.OK).json({ msg: "Product has been removed!" });
+};
 
 module.exports = {
   getAllProducts,
