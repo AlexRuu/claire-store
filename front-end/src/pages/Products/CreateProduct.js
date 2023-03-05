bb33eb9da8adecd274cc66101ae9c51a96049bbf
AlexRuu <weiru@hotmail.ca>
2023-03-04 21:25:09 -0500

    Add basic create product form structure

--- front-end/src/pages/Products/CreateProduct.js
+++ front-end/src/pages/Products/CreateProduct.js
@@ -11,6 +11,10 @@ const CreateProduct = () => {
   });
   const [designValue, setDesignValue] = useState("");
   const [design, setDesign] = useState([]);
+  const [styleValue, setStyleValue] = useState("");
+  const [style, setStyle] = useState([]);
+  const [detailsValue, setDetailsValue] = useState("");
+  const [details, setDetails] = useState([]);
 
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -20,10 +24,38 @@ const CreateProduct = () => {
     setDesignValue(e.target.value);
   };
 
-  const addRow = () => {
-    design.push(designValue);
-    console.log(design);
+  const handleStyleChange = (e) => {
+    setStyleValue(e.target.value);
   };
+
+  const handleDetailsChange = (e) => {
+    setDetailsValue(e.target.value);
+  };
+
+  const addDesign = (e) => {
+    e.preventDefault();
+    design.push(designValue);
+    setDesignValue("");
+  };
+
+  const addStyle = (e) => {
+    e.preventDefault();
+    style.push(styleValue);
+    setStyleValue("");
+  };
+
+  const addDetails = (e) => {
+    e.preventDefault();
+    details.push(detailsValue);
+    setDetailsValue("");
+  };
+
+  const createProduct = (e) => {
+    e.preventDefault();
+    const { name, price } = values;
+    setToCreate({ name, price, design, style });
+  };
+
   return (
     <div>
       <h1>Add a New Product</h1>
@@ -47,8 +79,41 @@ const CreateProduct = () => {
           value={designValue}
           onChange={handleDesignChange}
         />
+        <button onClick={addDesign}>Add</button>
+        <ul>
+          {design.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+        <FormRow
+          type="text"
+          name="style"
+          value={styleValue}
+          onChange={handleStyleChange}
+        />
+        <button onClick={addStyle}>Add</button>
+        <ul>
+          {style.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+        <FormRow
+          type="text"
+          name="details"
+          value={detailsValue}
+          onChange={handleDetailsChange}
+        />
+        <button onClick={addDetails}>Add</button>
+        <ul>
+          {details.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+        <FormRow type="file" />
+        <button type="submit" onClick={createProduct}>
+          Create
+        </button>
       </form>
-      <button onClick={addRow}>Add</button>
     </div>
   );
 };
